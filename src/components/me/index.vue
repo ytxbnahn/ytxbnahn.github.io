216f3e08c673689c9601683dfb512b1ddf1f84e4
971442841@qq.com <971442841@qq.com>
2017-08-14 11:04:11 +0800

    some qusertion

--- src/components/me/index.vue
+++ src/components/me/index.vue
@@ -12,15 +12,42 @@ export default {
     return {
       msg: [{
           'id': 1
+      }],
+      ll: '',
+      pp: [{'id': 2}]
+    }
+  },
+  computed: {
+    tt: function () {
+        console.log('dd')
+      this.ll = [{
+        'id': 2,
+        'd': 3
       }]
+      var a = JSON.stringify(this.ll)
+      var b = JSON.parse(a)
+      console.log('ddddddddddddddd222dd' + JSON.stringify(this.pp))
+      this.pp = this.pp.slice(1, 2)[0]
+      console.log('ddddddddd333ddd-ddddd' + JSON.stringify(this.pp))
+      return b
     }
   },
   methods: {
       demo: function() {
-          console.log('ddddddddddd')
+         const tt = [{
+          'id': 1,
+          'd': 6
+        }]
+        console.log('=222====dd====dddddddddddd')
+        console.log('ddddddddddd' + JSON.stringify(tt))
+        this.msg = this.tt
+      //  console.log(JSON.stringify(pp))
+        // this.msg = pp
+      //  this.msg[0].id = '222'
 //          this.msg[0].show = 'ddd'
         this.$set(this.msg[0], 'show', 'ddd')
         console.log(JSON.stringify(this.msg))
+        console.log(JSON.stringify(this.ll))
       }
   }
 }
